1a63fc7b8babfca5abe16988b4b765dd80f8468a
Hackman-git <abdulaigbenga96@gmail.com>
2020-12-07 10:48:28 -0600

    training successful

--- modeling.py
+++ modeling.py
@@ -6,6 +6,7 @@ import logging
 import re
 import os
 import tensorflow as tf
+
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import ModelCheckpoint
@@ -15,13 +16,17 @@ from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 from sklearn.model_selection import train_test_split
 
 
+IMAGE_SIZE = (180,180)
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+EPOCHS = 25
+INPUT_TENSOR_NAME = 'inputs_input'
+
 def conv_block(filters):
     block = tf.keras.Sequential([
         tf.keras.layers.SeparableConv2D(filters, 3, activation='relu', padding='same'),
         tf.keras.layers.SeparableConv2D(filters, 3, activation='relu', padding='same'),
         tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool2D()]
-    )
+        tf.keras.layers.MaxPool2D()])
     
     return block
 
@@ -40,9 +45,8 @@ def keras_model_fn(learning_rate, weight_decay, optimizer):
     TensorFlow Serving SavedModel at the end of training.
     """
     model = Sequential([
-        tf.keras.Input(shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)),
         
-        tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same', name='inputs',input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)),
         tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same'),
         tf.keras.layers.MaxPool2D(),
         
@@ -64,16 +68,14 @@ def keras_model_fn(learning_rate, weight_decay, optimizer):
     ])
 
     if optimizer.lower() == 'sgd':
-        opt = SGD(lr=learning_rate * size, decay=weight_decay, momentum=momentum)
+        opt = SGD(lr=learning_rate , decay=weight_decay, momentum=momentum)
     elif optimizer.lower() == 'rmsprop':
-        opt = RMSprop(lr=learning_rate * size, decay=weight_decay)
+        opt = RMSprop(lr=learning_rate , decay=weight_decay)
     else:
-        opt = Adam(lr=learning_rate * size, decay=weight_decay)
+        opt = Adam(lr=learning_rate, decay=weight_decay)
     
 
-    METRICS = ['accuracy',
-    tf.keras.metrics.Precision(name='precision'),
-    tf.keras.metrics.Recall(name='recall')]
+    METRICS = ['accuracy', tf.keras.metrics.Precision(name='precision'),tf.keras.metrics.Recall(name='recall')]
     
     model.compile(loss='binary_crossentropy',
                   optimizer=opt,
@@ -81,60 +83,39 @@ def keras_model_fn(learning_rate, weight_decay, optimizer):
     
     return model
 
-def _input(epochs, batch_size, channel_name, shuffle_buffer_size=1000):
-    obj = _get_datasets(channel_name)
+def _input(epochs, batch_size, channel, channel_name, shuffle_buffer_size=1000):
+    obj = _get_datasets(channel, channel_name)
     dataset = obj[0]
-    TRAIN_IMG_COUNT,VAL_IMG_COUNT, TEST_IMAGE_COUNT = obj[1],obj[2], obj[3]
+    TRAIN_IMG_COUNT,VAL_IMG_COUNT, TEST_IMAGE_COUNT, class_weight = obj[1],obj[2], obj[3],obj[4]
     
     dataset = dataset.shuffle(buffer_size=shuffle_buffer_size)
     # Repeat infinitely.
     dataset = dataset.repeat()
     
-    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.prefetch(10)
+    dataset = dataset.batch(args.batch_size, drop_remainder=True)
     dataset = dataset.prefetch(buffer_size=AUTOTUNE)
+
+    iterator = tf.compat.v1.data.make_one_shot_iterator(dataset)
     
-    image_batch, label_batch = next(iter(dataset))
+    image_batch, label_batch = iterator.get_next()
     
 
-    return ({INPUT_TENSOR_NAME: image_batch}, label_batch, TRAIN_IMG_COUNT, VAL_IMG_COUNT,TEST_IMAGE_COUNT)
+    return ({INPUT_TENSOR_NAME: image_batch}, label_batch, TRAIN_IMG_COUNT, VAL_IMG_COUNT,TEST_IMAGE_COUNT, class_weight)
 
 def train_input_fn():
-    return _input(args.epochs, args.batch_size, 'train')
+    return _input(args.epochs, args.batch_size, args.train, 'train')
 
 def eval_input_fn():
-    return _input(args.epochs, args.batch_size, 'eval')
+    return _input(args.epochs, args.batch_size, args.eval, 'eval')
 
 def validation_input_fn():
-    return _input(args.epochs, args.batch_size, 'validation')
-
-def _prepare_for_training(ds, cache=True, shuffle_buffer_size=1000):
-    # This is a small dataset, only load it once, and keep it in memory.
-    # use `.cache(filename)` to cache preprocessing work for datasets that don't
-    # fit in memory.
-    if cache:
-        if isinstance(cache, str):
-            ds = ds.cache(cache)
-        else:
-            ds = ds.cache()
-
-    ds = ds.shuffle(buffer_size=shuffle_buffer_size)
-
-    # Repeat forever
-    ds = ds.repeat()
-
-    ds = ds.batch(BATCH_SIZE)
-
-    # `prefetch` lets the dataset fetch batches in the background while the model
-    # is training.
-    ds = ds.prefetch(buffer_size=AUTOTUNE)
-
-    return ds
+    return _input(args.epochs, args.batch_size, args.validation, 'validation')
 
 
-def _get_datasets(channel_name):
-    filenames = tf.io.gfile.glob(r'./chest_xray/train/*/*')
-    filenames.extend(tf.io.gfile.glob(r'./chest_xray/val/*/*'))
-
+def _get_datasets(channel, channel_name):
+    filenames = tf.io.gfile.glob(os.path.join(args.train, '*/*'))
+    filenames.extend(tf.io.gfile.glob(os.path.join(args.validation, '*/*')))
     train_filenames, val_filenames = train_test_split(filenames, test_size=0.2)
     
     train_normal_count = len([filename for filename in train_filenames if "NORMAL" in filename])
@@ -149,17 +130,24 @@ def _get_datasets(channel_name):
     TRAIN_IMG_COUNT = tf.data.experimental.cardinality(train_list_ds).numpy()
     VAL_IMG_COUNT = tf.data.experimental.cardinality(val_list_ds).numpy()
     
-    test_list_ds = tf.data.Dataset.list_files(r'./chest_xray/test/*/*')
+     
+    test_filenames = tf.io.gfile.glob(os.path.join(args.eval, '*/*'))
+    test_list_ds = tf.data.Dataset.from_tensor_slices(test_filenames)
     TEST_IMAGE_COUNT = tf.data.experimental.cardinality(test_list_ds).numpy()
     test_ds = test_list_ds.map(_process_path)
-    test_ds = test_ds.batch(BATCH_SIZE)
+    
+    weight_for_0 = (1 / train_normal_count)*(TRAIN_IMG_COUNT)/2.0 
+    weight_for_1 = (1 / train_abnormal_count)*(TRAIN_IMG_COUNT)/2.0
+    class_weight = {0: weight_for_0, 1: weight_for_1}
+    
+    print('in get_ds function: '+str(TRAIN_IMG_COUNT)+' '+str(VAL_IMG_COUNT)+' '+str(TEST_IMAGE_COUNT), flush=True)
     
     if channel_name == 'train':
-        return (train_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT, TEST_IMAGE_COUNT)
-    elif channel_name == 'validation':
-        return (val_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT,TEST_IMAGE_COUNT)
-    else:
-        return (test_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT,TEST_IMAGE_COUNT)
+        return (train_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT, TEST_IMAGE_COUNT, class_weight)
+    if channel_name == 'validation':
+        return (val_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT,TEST_IMAGE_COUNT, class_weight)
+    if channel_name == 'eval':
+        return (test_ds,TRAIN_IMG_COUNT,VAL_IMG_COUNT,TEST_IMAGE_COUNT, class_weight)
     
     
     
@@ -186,12 +174,20 @@ def _process_path(file_path):
     img = _decode_img(img)
     return img, label
 
+def save_model(model, output):
+    
+    # create a TensorFlow SavedModel for deployment to a SageMaker endpoint with TensorFlow Serving
+    tf.saved_model.save(model, output+'/1/') 
+    logging.info("Model successfully saved at: {}".format(output))
+    return
+
 
 def main(args):
     logging.info("getting data")
     obj = train_input_fn()
     train_dataset = obj[0:2]
-    TRAIN_IMG_COUNT,VAL_IMG_COUNT, TEST_IMG_COUNT = obj[2:]
+    TRAIN_IMG_COUNT,VAL_IMG_COUNT,TEST_IMG_COUNT,class_weight = obj[2:]
+    
     eval_dataset = eval_input_fn()[0:2]
     validation_dataset = validation_input_fn()[0:2]
     
@@ -207,52 +203,64 @@ def main(args):
     size = 1
     
     model.fit(
-        train_dataset,
+        train_dataset[0],
+        train_dataset[1],
         steps_per_epoch=TRAIN_IMG_COUNT // args.batch_size,
         epochs=args.epochs,
         validation_data=validation_dataset,
         validation_steps=VAL_IMG_COUNT // args.batch_size,
-        class_weight=args.class_weight,
+        class_weight=class_weight,
         callbacks=callbacks)
 
 
-    score = model.evaluate(eval_dataset,
-            steps= TEST_IMG_COUNT // args.batch_size,
-            verbose=0)
+    score = model.evaluate(eval_dataset[0],
+                           eval_dataset[1],
+                        steps= TEST_IMG_COUNT // args.batch_size,
+                        verbose=0)
 
     logging.info('Test loss:{}'.format(score[0]))
     logging.info('Test accuracy:{}'.format(score[1]))
     logging.info('Test precision:{}'.format(score[2]))
     logging.info('Test recall:{}'.format(score[3]))
+    
+    return save_model(model, args.model_output_dir)
 
     
 if __name__ == '__main__':
-    aws s3 cp s3://sagemaker-pneumonia-bucket/chest_xray ./chest_xray --recursive;
-        
-    IMAGE_SIZE = (180,180)
-    BATCH_SIZE = 128
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    EPOCHS = 25
-    INPUT_TENSOR_NAME = 'inputs_input'
-    
-    import json
-    filenames = tf.io.gfile.glob(r'./chest_xray/train/*/*')
-    filenames.extend(tf.io.gfile.glob(r'./chest_xray/val/*/*'))
-    train_filenames, val_filenames = train_test_split(filenames, test_size=0.2)
-    train_list_ds = tf.data.Dataset.from_tensor_slices(train_filenames)
-    TRAIN_IMG_COUNT = tf.data.experimental.cardinality(train_list_ds).numpy()
-    train_normal_count = len([filename for filename in train_filenames if "NORMAL" in filename])
-    train_abnormal_count = len([filename for filename in train_filenames if "PNEUMONIA" in filename])
-    weight_for_0 = (1 / train_normal_count)*(TRAIN_IMG_COUNT)/2.0 
-    weight_for_1 = (1 / train_abnormal_count)*(TRAIN_IMG_COUNT)/2.0
-    class_weight = {0: weight_for_0, 1: weight_for_1}
     
     parser = argparse.ArgumentParser()
     
     parser.add_argument(
-        '--output_dir',
+        '--train',
         type=str,
-        default= './')
+        required=False,
+        default=os.environ.get('SM_CHANNEL_TRAIN'),
+        help='The directory where the input data is stored.')
+    parser.add_argument(
+        '--validation',
+        type=str,
+        required=False,
+        default=os.environ.get('SM_CHANNEL_VALIDATION'),
+        help='The directory where the input data is stored.')
+    parser.add_argument(
+        '--eval',
+        type=str,
+        required=False,
+        default=os.environ.get('SM_CHANNEL_EVAL'),
+        help='The directory where the input data is stored.')
+    parser.add_argument(
+        '--output-dir',
+        type=str,
+        default=os.environ.get('SM_OUTPUT_DIR'))
+    parser.add_argument(
+        '--model_dir',
+        type=str,
+        required=True,
+        help='The directory where the model will be stored.')
+    parser.add_argument(
+        '--model_output_dir',
+        type=str,
+        default=os.environ.get('SM_MODEL_DIR'))
     parser.add_argument(
         '--weight_decay',
         type=float,
@@ -266,12 +274,12 @@ if __name__ == '__main__':
     parser.add_argument(
         '--epochs',
         type=int,
-        default=EPOCHS,
+        default=1,
         help='The number of steps to use for training.')
     parser.add_argument(
         '--batch_size',
         type=int,
-        default=BATCH_SIZE,
+        default=128,
         help='Batch size for training.')
     parser.add_argument(
         '--optimizer',
@@ -282,11 +290,6 @@ if __name__ == '__main__':
         '--momentum',
         type=float,
         default='0.9'
-    )
-    parser.add_argument(
-        '--class_weight',
-        type = json.loads,
-        default=class_weight
     )
     
     args = parser.parse_known_args()[0]
